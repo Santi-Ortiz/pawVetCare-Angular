2fab7bcf4f0280f86524592a188e10d0eadfbcfc
Santi-Ortiz <106709063+Santi-Ortiz@users.noreply.github.com>
2024-10-15 19:55:15 -0500

    Buscar cliente por cedula en paginación de clientes

--- PawVetCare/src/app/cliente/paginacion-clientes/paginacion-clientes.component.ts
+++ PawVetCare/src/app/cliente/paginacion-clientes/paginacion-clientes.component.ts
@@ -14,7 +14,17 @@ export class PaginacionClientesComponent {
   itemsPerPage: number = 4; // Número de clientes a mostrar por página
   totalPages: number = 1; // Total de páginas calculadas
   clientes: Cliente[] = []; // Arreglo que almacenará la lista de clientes
+  cedulaCliente: number | undefined; // ID del cliente que se buscará
 
+  // Datos para un nuevo cliente que se agregará al sistema
+  nuevoCliente: Cliente = {
+    id: 0,
+    cedula: 0,
+    nombre: '',
+    correo: '',
+    celular: 0,
+    mascotas: [],
+  };
   // Inyectamos el servicio ClienteService y Router en el constructor
   constructor(private clientesService: ClienteService, private router: Router) {}
 
@@ -39,6 +49,40 @@ export class PaginacionClientesComponent {
         },
         (error) => {
           console.error('Error al cargar los clientes del veterinario:', error);
+        }
+      );
+    }
+  }
+
+  buscarCliente(cedulaCliente: number | undefined): void {
+    const cedula = Number(cedulaCliente); // Convierte la cédula a número
+  
+    if (!cedula) { // Si la cédula es inválido, muestra un mensaje de error
+      alert('Cedula de cliente inválida.');
+      return;
+    }
+  
+    // Si el usuario es admin, realiza la búsqueda de cliente por cedula
+    if (this.userType === 'admin') {
+      this.clientesService.obtenerClientePorCedula(cedula).subscribe(
+        (cliente: Cliente) => {
+          this.nuevoCliente = cliente; // Asigna el cliente encontrado
+          this.router.navigate(['/cliente', cedula]); // Navega a la página del cliente con esa cédula
+        },
+        (error) => { // Muestra error si no se encuentra el cliente
+          console.error(`Error al obtener la mascota para admin con ID ${cedula}:`, error);
+          alert(`Mascota con ID ${cedula} no encontrada`);
+        }
+      );
+    } else if (this.userType === 'vet') { // Lo mismo para veterinarios
+      this.clientesService.obtenerClientePorCedula(cedula).subscribe(
+        (cliente: Cliente) => {
+          this.nuevoCliente = cliente;
+          this.router.navigate(['/cliente', cedula]); 
+        },
+        (error) => {
+          console.error(`Error al obtener la cédula para vet con cedula ${cedula}:`, error);
+          alert(`Cliente con cedula ${cedula} no encontrada`);
         }
       );
     }
